f6963a9f42d9ddb2c43b02e56b603ed3dda51a93
Richard Deodutt <richarddeodutt@gmail.com>
2022-08-16 22:54:21 -0400

    Missing comments, Added more colors and change the colors up for more variety

--- Chronicles-of-Kura.sh
+++ Chronicles-of-Kura.sh
@@ -10,6 +10,9 @@
 Red='\033[0;31m'
 Green='\033[0;32m'
 Yellow='\033[0;33m'
+Blue='\033[0;34m'
+Purple='\033[0;35m'
+Cyan='\033[0;36m' 
 #No color output, don't change
 NC='\033[0m'
 
@@ -39,12 +42,12 @@ gameover(){
 #Write the credits of the game creators here
 rollcredits(){
     #Print the credits
-    print "Created by Agbai Anya(Team Leader)(finalboss360)" $Green
-    print "Created by Brian Louie(Presenter)(kura3git)" $Green
-    print "Created by Richard Deodutt(Supervisor)(RichardDeodutt)" $Green
-    print "Created by Lora Lee Barreto(Documenter)(LLBArch7)" $Green
-    print "Created by Clarence Munn(Documenter)(munney84)" $Green
-    print "Inspired by NetworkChuck and Elden Ring" $Green
+    print "Created by Agbai Anya(Team Leader)(finalboss360)" $Purple
+    print "Created by Brian Louie(Presenter)(kura3git)" $Purple
+    print "Created by Richard Deodutt(Supervisor)(RichardDeodutt)" $Purple
+    print "Created by Lora Lee Barreto(Documenter)(LLBArch7)" $Purple
+    print "Created by Clarence Munn(Documenter)(munney84)" $Purple
+    print "Inspired by NetworkChuck and Elden Ring" $Purple
 }
 
 #Game clear function, call this when the player wins
@@ -59,6 +62,7 @@ gameclear(){
 
 #Welcome to the game function, call this when the player starts the game
 welcome(){
+    #Send the player a welcome message
     print "Welcome to The Chronicles of Kura" $Green
 }
 
@@ -69,23 +73,24 @@ battle(){
     Enemy=$(shuf -i 0-1 -n1)
     #Player selection translated as 0 or 1, defaults to 0 until assigned
     Player=0
-    #Get player to pick heads or tails
-    printf "${Yellow}%s${NC}" "Lets flip a coin, pick heads or tails: H/T ? "
+    #Ask the player to pick heads or tails
+    printf "${Cyan}%s${NC}" "Lets flip a coin, pick heads or tails: H/T ? "
+    #read the player's response
     read Flip
     #Only get the lowercase of the first character to check if heads or tails
     Flip=$(printf "%s" $Flip | tr [:upper:] [:lower:] | cut -c 1)
     #if response is 'h' it means 'heads' if is 't' it means 'tails'
     if [[ $Flip == 'h' ]]; then
         #player selected heads assign player to 0 for heads
-        print "You selected heads" $Yellow
+        print "You selected heads" $Cyan
         Player=0
     elif [[ $Flip == 't' ]]; then
         #player selected tails assign player to 1 for tails
-        print "You selected tails" $Yellow
+        print "You selected tails" $Cyan
         Player=1
     else
         #Flip was not understood so assume heads and assign player to 0 for heads
-        print "Did not understand that assuming heads" $Yellow
+        print "Did not understand that assuming heads" $Cyan
         Player=0
     fi
     #Check if player guessed the random selection of the enemy
